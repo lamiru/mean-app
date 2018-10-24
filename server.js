ce57f31485e2389472b871e6df11325e24c165a2
lamiru <lamiru@gmail.com>
2018-10-24 09:52:34 +0900

    server.js 추가
    npm install -g nodemon
    nodemon server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.use('/$', function(req, res) {
+  res.send('Hello World')
+})
+app.listen(3000)
+module.exports = app
+
+console.log('Server running at http://localhost:3000/')
