d3d7d041a8abdda05aef2643bf845fa1ef213d5d
lamiru <lamiru@gmail.com>
2018-10-25 12:48:18 +0900

    /users GET 추가

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,5 +1,16 @@
 var User = require('mongoose').model('User')
 
+exports.index = function(req, res, next) {
+  // User.find({}, function(err, users) { 조건 없이 전체 조회
+  User.find({}, 'username firstName lastName email', { skip: 0, limit: 1000 }, function(err, users) {
+    if (err) {
+      return next(err)
+    } else {
+      res.json(users)
+    }
+  })
+}
+
 exports.new = function(req, res, next) {
   if (req.method == 'POST') {
     var user = new User(req.body)
--- app/routes/users.server.route.js
+++ app/routes/users.server.route.js
@@ -1,6 +1,9 @@
 var users = require('../../app/controllers/users.server.controller')
 
 module.exports = function(app) {
+  app.route('/users$')
+    .get(users.index)
+
   app.route('/users/new$')
     .post(users.new)
     .get(users.new)
